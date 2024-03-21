a814d7255810301423e802df12ebb392d00981e3
Daniel Thompson <danielt3131@gmail.com>
2024-03-21 17:03:56 -0400

    Created a reference class file for all static values.

---
+++ app/src/main/java/io/github/danielt3131/fsm/Reference.java
@@ -0,0 +1,11 @@
+package io.github.danielt3131.fsm;
+
+public class Reference {
+    static final int SEGMENT_SIZE_EMAIL_PRESET = 20000000; // 20MB
+    static final int SEGMENT_SIZE_MMS_PRESET = 1000000;
+    static final int INPUT_FILE = 10;
+    static final String SAVE_LOCATION = "/storage/emulated/0/Documents/FSM/";
+    static int segmentSize = 0;
+    static int MAX_BUFFFERSIZE = 1024 * 1024 * 250;
+
+}
